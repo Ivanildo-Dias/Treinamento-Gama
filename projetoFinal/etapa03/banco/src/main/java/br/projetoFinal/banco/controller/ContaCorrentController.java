5f247a1aa253d8db191874064427dfd1e81ce3c1
Ivanildo Dias Rocha <ivanildo.rocha@outlook.com>
2022-03-25 12:24:00 -0300

    Ajuste do método Adicionar e Deletar

--- projetoFinal/etapa03/banco/src/main/java/br/projetoFinal/banco/controller/ContaCorrentController.java
+++ projetoFinal/etapa03/banco/src/main/java/br/projetoFinal/banco/controller/ContaCorrentController.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.projetoFinal.banco.dao.ContaCorrenteDAO;
@@ -13,19 +15,32 @@ import br.projetoFinal.banco.model.ContaCorrente;
 
 @CrossOrigin
 @RestController
+
 public class ContaCorrentController {
 
     @Autowired
 	private ContaCorrenteDAO dao;
-	@GetMapping("/conta-corrente")
+
+	@GetMapping("/conta-corrente/buscar")
 	public ArrayList<ContaCorrente> recuperarTudasContasCorrente(){
 		ArrayList<ContaCorrente> lista;
 		lista = (ArrayList<ContaCorrente>)dao.findAll();
 		return lista;
 		}
 	
-	@GetMapping("/conta-corrente/{numero}")
+	@GetMapping("/conta-corrente/buscar/{numero}")
 	public ContaCorrente recuperarContaCorrente(@PathVariable int numero) {
 		return dao.findById(numero).orElse(null);
 	}
+
+	@PostMapping("/conta-corrente/cadastrar")
+	public ContaCorrente criarContaCorrente(ContaCorrente contaCorrente) {
+		return dao.save(contaCorrente);
+	}
+
+	@DeleteMapping("/conta-corrente/deletar/{numero}")
+	public void deletarContaCorrente(@PathVariable int numero) {
+		dao.deleteById(numero);
+	}
+
 }
--- projetoFinal/etapa03/banco/src/main/java/br/projetoFinal/banco/controller/ContaEspecialController.java
+++ projetoFinal/etapa03/banco/src/main/java/br/projetoFinal/banco/controller/ContaEspecialController.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.projetoFinal.banco.dao.ContaEspecialDAO;
@@ -13,18 +15,31 @@ import br.projetoFinal.banco.model.ContaEspecial;
 
 @CrossOrigin
 @RestController
+
 public class ContaEspecialController {
     @Autowired
 	private ContaEspecialDAO dao;
-	@GetMapping("/conta-especial")
+	
+	@GetMapping("/conta-especial/buscar")
 	public ArrayList<ContaEspecial> recuperarTodasContasEspecial(){
 		ArrayList<ContaEspecial> lista;
 		lista = (ArrayList<ContaEspecial>)dao.findAll();
 		return lista;
 		}
 	
-	@GetMapping("/conta-especial/{numero}")
+	@GetMapping("/conta-especial/buscar/{numero}")
 	public ContaEspecial recuperarContaEspecial(@PathVariable int numero) {
 		return dao.findById(numero).orElse(null);
 	}
+
+	@PostMapping("/conta-especial/cadastrar")
+	public ContaEspecial criarContaEspecial(ContaEspecial contaEspecial) {
+		return dao.save(contaEspecial);
+	}
+
+	@DeleteMapping("/conta-especial/deletar/{numero}")
+	public void deletarContaEspecial(@PathVariable int numero) {
+		dao.deleteById(numero);
+	}
+
 }
